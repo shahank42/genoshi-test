1e14100aacc9615272f29d7b0b428d089994b77a
shahank42 <shounakghosh42@gmail.com>
2023-12-30 16:27:08 +0530

    feat(list): Easy search

--- src/components/UserGraphList.tsx
+++ src/components/UserGraphList.tsx
@@ -1,15 +1,26 @@
+"use client";
+
 import { UserGraph } from "@/lib/types";
 import UserGraphListItem from "./UserGraphListItem";
 import { Button, buttonVariants } from "./ui/button";
 import { Input } from "./ui/input";
 import { USER_GRAPHS } from "@/lib/config";
 import Link from "next/link";
+import { useState } from "react";
 
 const UserGraphList = () => {
+  const [search, setSearch] = useState<string>("");
+
   return (
     <div className="flex flex-col gap-3 lg:gap-5">
       <div className="flex flex-col-reverse lg:flex-row gap-5 justify-between">
-        <Input placeholder="Search my graphs..." />
+        <Input
+          placeholder="Search my graphs..."
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
         {/* <Button>New Graph</Button> */}
         <Link
           href="/create-new-graph"
@@ -19,7 +30,7 @@ const UserGraphList = () => {
         </Link>
       </div>
       <div className="flex flex-col gap-3 justify-center md:grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 md:gap-x-2 md:gap-y-2">
-        {USER_GRAPHS.map((graph) => {
+        {(search === "" ? USER_GRAPHS : USER_GRAPHS.filter((graph) => graph.title.toLowerCase().includes(search))).map((graph) => {
           return <UserGraphListItem key={graph.id} data={graph} />;
         })}
       </div>
