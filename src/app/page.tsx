61d57d6ce377e9a36f3c197151c42dafbb876941
shahank42 <shounakghosh42@gmail.com>
2023-12-30 16:08:13 +0530

    chore(ui): Fixed initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div><Button>Leggo</Button></div>
-  )
+    <div className="w-full flex justify-evenly h-[100dvh] items-center">
+      <div className="flex flex-col gap-5">
+        <Link
+          className={cn(buttonVariants({ variant: "default" }))}
+          href="/profile"
+        >
+          Go to profile
+        </Link>
+        <Link
+          className={cn(buttonVariants({ variant: "default" }))}
+          href="/pricing"
+        >
+          Go to pricing
+        </Link>
+      </div>
+    </div>
+  );
 }
